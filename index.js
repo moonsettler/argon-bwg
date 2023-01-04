b124b505052fa04ee66f8ca8efd287ae08627598
moonsettler <moonsettler@protonmail.com>
2023-01-04 01:12:05 +0100

    calculate sha256 from file
    added: can browse or drop file to automatically get the entropy base
    removed: test message and salt

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-document.getElementById('message').value = '5C3044FE3445452CC3C5A166D1EFF67182AD799362CFD207033F2DFFF1058868';
-document.getElementById('salt').value = 'silver basic have size pill labor';
+document.getElementById('message').value = '';
+document.getElementById('salt').value = '';
 document.getElementById('p').value = '2';
 document.getElementById('m').value = '1024';
 document.getElementById('i').value = '60';
 document.getElementById('l').value = '32';
+
+document.getElementById('file').addEventListener('change', hashfile);
 
 document.getElementById('start').addEventListener('click', () => {
 	let message = document.getElementById('message').value;
@@ -53,4 +55,28 @@ function bytesToHex(bytes) {
         hex.push((current & 0xF).toString(16));
     }
     return hex.join("");
+}
+
+// Calculate SHA256 of a file selected
+function hashfile() {
+	let fileselector = document.getElementById('file');
+	readbinaryfile(fileselector.files[0])
+		.then(function (result) {
+			result = new Uint8Array(result);
+			return window.crypto.subtle.digest('SHA-256', result);
+		}).then(function (result) {
+			result = new Uint8Array(result);
+			var resulthex = bytesToHex(result);
+			document.getElementById('message').value = resulthex;
+		});
+}
+
+function readbinaryfile(file) {
+	return new Promise((resolve, reject) => {
+		var fr = new FileReader();
+		fr.onload = () => {
+			resolve(fr.result)
+		};
+		fr.readAsArrayBuffer(file);
+	});
 }
